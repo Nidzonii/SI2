b30adabe604464f43eb5f1c7346633bc1edfdfef
sofijajakovljevic <socaacos96j@gmail.com>
2019-02-09 20:04:59 +0100

    Promenjena strana AdminDodavanjeKorisnika: -combobox dodat -kad se doda korisnik brisu se polja

--- StoreSoftware/KonekcioniString.cs
+++ StoreSoftware/KonekcioniString.cs
@@ -9,11 +9,12 @@ namespace StoreSoftware
 {
     class KonekcioniString
     {
-        private static SqlConnection konekcija = new SqlConnection("Data Source=LAPTOP-QQ1TU8ST;Initial Catalog=ProdavnicaRacunarskeOpreme;Integrated Security=True");
+        private static SqlConnection konekcija = new SqlConnection("Data Source=DESKTOP-OSVDU0O;Initial Catalog=ProdavnicaRacunarskeOpreme;Integrated Security=True");
 
         public static SqlConnection getKonekcija()
         {
             return konekcija;
         }
     }
+   
 }
